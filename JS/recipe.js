917082ef097dcb8c381db18c22db9c77ed7e7ecb
githinjialbert <githinjialbert001@gmail.com>
2024-09-06 17:19:39 +0300

    Styling the recipeSection

--- JS/recipe.js
+++ JS/recipe.js
@@ -1,12 +1,13 @@
 const searchRecipe = document.getElementById("search-recipe");
 const searchBtn = document.getElementById("search-button");
 const recipeSection = document.getElementById("recipe-section");
-const recipeDetails = document.getElementById("recipe-details");
+
 
 const apiKey = 'da98749161094a97d0caaa27639b516d';
 const apiId = '518bdf4f';
 
 const fetchRecipe = async () => {
+
     try {
         const query = searchRecipe.value.trim();
         if (!query) {
@@ -18,11 +19,14 @@ const fetchRecipe = async () => {
         const apiUrl = `https://api.edamam.com/search?q=${encodeURIComponent(query)}&app_id=${apiId}&app_key=${apiKey}&from=0&to=9`;
 
         const response = await fetch(apiUrl);
+
         if (!response.ok) {
+
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
+
         console.log(data); 
 
         if (data && data.hits && data.hits.length > 0) {
@@ -41,7 +45,6 @@ const fetchRecipe = async () => {
                     <ul>
                     ${recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
                     </ul>
-                    <a href="${recipe.url}" target="_blank">View Recipe</a>
                 </div>
                 `;
             });
@@ -57,6 +60,7 @@ const fetchRecipe = async () => {
         console.error('Error fetching data:', error);
         recipeSection.innerHTML = "<p>Error fetching Recipe</p>";
     }
+    
 }
 
 searchBtn.addEventListener('click', fetchRecipe);
